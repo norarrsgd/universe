b851a2bcd6f97928579f033c7ef261dd3609ce63
nor arrsgd <norgelera@duck.com>
2025-07-10 20:58:00 +0800

    feat: add json ordering to base fields

--- code/Universe/Interfaces/ICosmicEntity.cs
+++ code/Universe/Interfaces/ICosmicEntity.cs
@@ -5,16 +5,20 @@ public interface ICosmicEntity
 {
     /// <summary>Unique GUID</summary>
 #pragma warning disable IDE1006 // Naming Styles
+    [JsonPropertyOrder(0)]
     public string id { get; set; }
 #pragma warning restore IDE1006 // Naming Styles
 
     /// <summary>UTC Date document was added</summary>
+    [JsonPropertyOrder(1)]
     public DateTime AddedOn { get; set; }
 
     /// <summary>UTC Date document was modified.</summary>
+    [JsonPropertyOrder(2)]
     public DateTime? ModifiedOn { get; set; }
 
     /// <summary>Represents the count of items in a query result.</summary>
+    [JsonPropertyOrder(3)]
     public long CountAggregate { get; set; }
 }
 
@@ -22,15 +26,19 @@ public interface ICosmicEntity
 public abstract record CosmicEntity : IDisposable, ICosmicEntity
 {
     /// <inheritdoc />
+    [JsonPropertyOrder(0)]
     public string id { get; set; } = Guid.NewGuid().ToString();
 
     /// <inheritdoc />
+    [JsonPropertyOrder(1)]
     public DateTime AddedOn { get; set; } = DateTime.UtcNow;
 
     /// <inheritdoc />
+    [JsonPropertyOrder(2)]
     public DateTime? ModifiedOn { get; set; } = DateTime.UtcNow;
 
     /// <inheritdoc />
+    [JsonPropertyOrder(3)]
     public long CountAggregate { get; set; }
 
     #region Dispose Pattern
